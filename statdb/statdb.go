1e2002632495f721358dbebfa4505ea222bf6aac
cuit-lilang <lim92399@gmail.com>
2024-06-28 22:40:36 +0800

    Update statdb.go

--- statdb/statdb.go
+++ statdb/statdb.go
@@ -2,10 +2,10 @@ package statdb
 
 import (
 	"cxchain223/kvstore"
-	"cxchain223/trie"
 	"cxchain223/types"
 	"cxchain223/utils/hash"
 	"cxchain223/utils/rlp"
+	"fmt"
 )
 
 type StatDB interface {
@@ -13,27 +13,48 @@ type StatDB interface {
 	Load(addr types.Address) *types.Account
 	Store(addr types.Address, account types.Account)
 }
-
-type statedbroot struct {
-	state *trie.State
+type IStatDB struct {
+	//Trie trie.ITrie
+	Trie *kvstore.LevelDB
 }
 
-func (sta statedbroot) SetStatRoot(root hash.Hash) {
-	var db kvstore.KVDatabase
-	state := trie.NewState(db, root)
-	sta.state = state
+//	func NewIStatDB(db kvstore.KVDatabase, root hash.Hash) IStatDB {
+//		return IStatDB{
+//			trie.NewState(db,root),
+//		}
+//	}
+func NewIStatDB(path string) IStatDB {
+	return IStatDB{
+		Trie: kvstore.NewLevelDB(path),
+	}
 }
+func (i *IStatDB) SetStatRoot(root hash.Hash) {
+	//e := i.Trie.(*trie.State)
+	//e.
 
-func (sta statedbroot) Load(addr types.Address) *types.Account {
-	var byteaddr []byte
-	copy(byteaddr, addr[:])
-	accountValue, _ := sta.state.Load(byteaddr)
+}
+func (i *IStatDB) Load(addr types.Address) *types.Account {
+	//data, err := i.Trie.Load(addr[:])
+	data, err := i.Trie.Get(addr[:])
+	if err != nil {
+		fmt.Println("Bob addr:", addr)
+		panic(err)
+	}
 	var account types.Account
-	rlp.DecodeBytes(accountValue, account)
+	err = rlp.DecodeBytes(data, &account)
+	if err != nil {
+		panic(err)
+	}
 	return &account
 }
-
-func (sta statedbroot) Store(addr types.Address, account types.Account) {
-	accountValue, _ := rlp.EncodeToBytes(account)
-	sta.state.Store(addr[:], accountValue)
+func (i *IStatDB) Store(addr types.Address, account types.Account) {
+	val, err := rlp.EncodeToBytes(account)
+	if err != nil {
+		panic(err)
+	}
+	//err = i.Trie.Store(addr[:], val)
+	err = i.Trie.Put(addr[:], val)
+	if err != nil {
+		panic(err)
+	}
 }

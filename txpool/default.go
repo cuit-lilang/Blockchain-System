fe8b81bcd3dc68a09da0eceab94f4dea355c0fdd
cuit-lilang <lim92399@gmail.com>
2024-06-28 22:41:41 +0800

    Update default.go

--- txpool/default.go
+++ txpool/default.go
@@ -3,7 +3,7 @@ package txpool
 import (
 	"cxchain223/statdb"
 	"cxchain223/types"
-	"hash"
+	"cxchain223/utils/hash"
 	"sort"
 )
 
@@ -14,104 +14,263 @@ type SortedTxs interface {
 	Pop() *types.Transaction
 	Nonce() uint64
 }
+type arr_tx []DefaultSortedTxs
 
+func (txs arr_tx) Len() int {
+	return len(txs)
+}
+func (txs arr_tx) Less(i, j int) bool {
+	return txs[i].GasPrice() > txs[j].GasPrice()
+}
+func (txs arr_tx) Swap(i, j int) {
+	txs[i], txs[j] = txs[j], txs[i]
+}
+
+// these tx have a same gasPrice and addr.
 type DefaultSortedTxs []*types.Transaction
 
-func (sorted DefaultSortedTxs) GasPrice() uint64 {
-	first := sorted[0]
-	return first.GasPrice
+func (d *DefaultSortedTxs) GasPrice() uint64 {
+	tem := *d
+	return tem[0].GasPrice
+}
+func (d *DefaultSortedTxs) Push(tx *types.Transaction) {
+	tem := *d
+	tem = append(tem, tx)
+	*d = tem
+}
+func (d *DefaultSortedTxs) Pop() *types.Transaction {
+	tem := *d
+	if len(tem) == 0 {
+		return nil
+	}
+	res := tem[0]
+	if len(tem) < 2 {
+		tem = make(DefaultSortedTxs, 0)
+		*d = tem
+	} else {
+		*d = tem[1:]
+	}
+	return res
+}
+func (d *DefaultSortedTxs) Replace(tx *types.Transaction) {
+	tem := *d
+	for i, v := range tem {
+		if v.Nonce == tx.Nonce {
+			tem[i] = tx
+		}
+	}
+	*d = tem
+}
+func (d *DefaultSortedTxs) Nonce() uint64 {
+	tem := *d
+	return tem[len(tem)-1].Nonce
+}
+func (d *DefaultSortedTxs) Address() types.Address {
+	tem := *d
+	return tem[0].From()
 }
 
-type pendingTxs []SortedTxs
+type QueenSortedTxs []*types.Transaction
 
-func (p pendingTxs) Len() int {
-	return len(p)
+func (txs QueenSortedTxs) Len() int {
+	return len(txs)
+}
+func (txs QueenSortedTxs) Less(i, j int) bool {
+	return txs[i].Nonce < txs[j].Nonce
+}
+func (txs QueenSortedTxs) Swap(i, j int) {
+	txs[i], txs[j] = txs[j], txs[i]
 }
 
-func (p pendingTxs) Less(i, j int) bool {
-	return p[i].GasPrice() < p[j].GasPrice()
+type SubPool interface {
+	Push(tx *types.Transaction)
+	Pop() *types.Transaction
+	GasPrice() uint64
+	Nonce() uint64
+	Address() types.Address
+	Replace(tx *types.Transaction)
 }
 
-func (p pendingTxs) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
+type PendingTxs []SubPool
+
+func NewPendingTxs() PendingTxs {
+	res := make([]SubPool, 0)
+	return res
+}
+func (txs PendingTxs) Len() int {
+	return len(txs)
+}
+func (txs PendingTxs) Less(i, j int) bool {
+	if txs[i].Address() == txs[j].Address() {
+		return txs[i].Nonce() < txs[j].Nonce()
+	}
+	return txs[i].GasPrice() > txs[j].GasPrice()
+}
+func (txs PendingTxs) Swap(i, j int) {
+	txs[i], txs[j] = txs[j], txs[i]
 }
 
 type DefaultPool struct {
-	Stat statdb.StatDB
-
-	all      map[hash.Hash]bool
-	txs      pendingTxs
-	pendings map[types.Address][]SortedTxs
-	queue    map[types.Address][]*types.Transaction
+	StatDB  *statdb.IStatDB
+	all     map[hash.Hash]bool
+	txs     PendingTxs
+	pending map[types.Address]arr_tx
+	queue   map[types.Address]QueenSortedTxs
 }
 
-func (pool DefaultPool) SetStatRoot(root hash.Hash) {
-	pool.Stat.SetStatRoot(root)
+func (pool DefaultPool) Has() bool {
+	return len(pool.txs) > 0
 }
-
-func (pool DefaultPool) NewTx(tx *types.Transaction) {
-	account := pool.Stat.Load(tx.From())
+func (pool *DefaultPool) NewTx(tx *types.Transaction) {
+	from := tx.From()
+	account := pool.StatDB.Load(from)
 	if account.Nonce >= tx.Nonce {
 		return
 	}
 
 	nonce := account.Nonce
-	blks := pool.pendings[tx.From()]
-	if len(blks) > 0 {
-		last := blks[len(blks)-1]
+	pools, ok := pool.pending[from]
+	if !ok {
+		pools = make(arr_tx, 0)
+		pool.pending[from] = pools
+	}
+	if len(pools) > 0 {
+		last := pools[len(pools)-1]
 		nonce = last.Nonce()
 	}
 	if tx.Nonce > nonce+1 {
+		// 加到queue
 		pool.addQueueTx(tx)
 	} else if tx.Nonce == nonce+1 {
-		// push
-		pool.pushPendingTx(blks, tx)
+		// 加到pending，判断是否有queue的交易可以pop
+		pool.addPendingTx(tx)
 	} else {
-		// 替换
-		pool.replacePendingTx(blks, tx)
+		// replace
+		pool.replacePendingTx(tx)
 	}
 }
 
-func (pool DefaultPool) replacePendingTx(blks []SortedTxs, tx *types.Transaction) {
-	for _, blk := range blks {
-		if blk.Nonce() >= tx.Nonce {
-			// replace
-			if blk.GasPrice() <= tx.GasPrice {
-				blk.Replace(tx)
+func (pool *DefaultPool) addQueueTx(tx *types.Transaction) {
+	txs := pool.queue[tx.From()]
+	txs = append(txs, tx)
+	// TODO 对txs进行排序
+	sort.Sort(txs)
+	pool.queue[tx.From()] = txs
+}
+
+// tx's nonce is max in pending+1, make it in the back of txs
+func (pool *DefaultPool) addPendingTx(tx *types.Transaction) {
+	from := tx.From()
+	subpools := pool.pending[from]
+	if len(subpools) == 0 {
+		sub := make(DefaultSortedTxs, 0)
+		sub = append(sub, tx)
+		subpools = append(subpools, sub)
+		pool.txs = append(pool.txs, &sub)
+		pool.pending[from] = subpools
+		sort.Sort(pool.txs)
+	} else {
+		last := subpools[len(subpools)-1]
+		if last.GasPrice() <= tx.GasPrice {
+			last = append(last, tx)
+			pool.pending[from][len(subpools)-1] = last
+			for i, v := range pool.txs {
+				if v.Address() == from && v.Nonce() < tx.Nonce {
+					pool.txs[i].Push(tx)
+					break
+				}
+			}
+			sort.Sort(pool.txs)
+		} else {
+			sub := make(DefaultSortedTxs, 0)
+			sub = append(sub, tx)
+			subpools = append(subpools, sub)
+			pool.txs = append(pool.txs, &sub)
+			pool.pending[from] = subpools
+			sort.Sort(pool.txs)
+		}
+	}
+	// TODO 更新queue中可以pop到pending中的交易
+	quee := pool.queue[from]
+	sort.Search(quee.Len(), func(i int) bool {
+		if quee[i].Nonce == tx.Nonce+1 {
+			pool.addPendingTx(quee[i])
+		}
+		return true
+	})
+}
+
+func (pool *DefaultPool) replacePendingTx(tx *types.Transaction) {
+	subpools := pool.pending[tx.From()]
+	for _, sub := range subpools {
+		if sub.Nonce() >= tx.Nonce {
+			if tx.GasPrice >= sub.GasPrice() {
+				sub.Replace(tx)
 			}
 			break
 		}
 	}
 }
 
-func (pool DefaultPool) pushPendingTx(blks []SortedTxs, tx *types.Transaction) {
-	if len(blks) == 0 {
-		blk := make(DefaultSortedTxs, 1)
-		blk = append(blk, tx)
-		blks = append(blks, blk)
-		pool.pendings[tx.From()] = blks
-		pool.txs = append(pool.txs, blk)
-		sort.Sort(pool.txs)
-	} else {
-		last := blks[len(blks)-1]
-		if last.GasPrice() <= tx.GasPrice {
-			last.Push(tx)
+func (pool DefaultPool) Nonce(addr types.Address) uint64 {
+	return 1
+}
+
+func (pool *DefaultPool) Pop() *types.Transaction {
+	res := pool.txs[0].Pop()
+	x := pool.txs[0].(*DefaultSortedTxs)
+	if len(*x) == 0 {
+		if len(pool.txs) == 1 {
+			pool.txs = make(PendingTxs, 0)
 		} else {
-			blk := make(DefaultSortedTxs, 1)
-			blk = append(blk, tx)
-			blks = append(blks, blk)
-			pool.pendings[tx.From()] = blks
-			pool.txs = append(pool.txs, blk)
-			sort.Sort(pool.txs)
+			pool.txs = pool.txs[1:]
 		}
 	}
+	addr := res.From()
+	nonce := res.Nonce
+	for idx, v := range pool.pending[addr] {
+		for i, v2 := range v {
+			if v2.Nonce == nonce {
+				if len(v) == 1 {
+					if len(pool.pending[addr]) == 1 {
+						delete(pool.pending, addr)
+
+					} else {
+						if idx == len(pool.pending[addr])-1 {
+							pool.pending[addr] = pool.pending[addr][:idx]
+						} else if idx == 0 {
+							pool.pending[addr] = pool.pending[addr][1:]
+						} else {
+							pool.pending[addr] = append(pool.pending[addr][:idx], pool.pending[addr][idx+1:]...)
+
+						}
+					}
+				} else {
+					if i == 0 {
+						pool.pending[addr][idx] = pool.pending[addr][idx][1:]
+					} else if i == len(pool.pending[addr][idx])-1 {
+						pool.pending[addr][idx] = pool.pending[addr][idx][:i]
+					} else {
+						pool.pending[addr][idx] = append(pool.pending[addr][idx][:i], pool.pending[addr][idx][i+1:]...)
+					}
+				}
+				return res
+			}
+		}
+	}
+	return res
 }
 
-func (pool DefaultPool) addQueueTx(tx *types.Transaction) {
-	list := pool.queue[tx.From()]
-	list = append(list, tx)
-	// sort
-}
+func (pool DefaultPool) SetStatRoot(root hash.Hash) {}
 
-func (pool DefaultPool) Pop() *types.Transaction                {}
 func (pool DefaultPool) NotifyTxEvent(txs []*types.Transaction) {}
+
+func NewDefaultPool(db statdb.IStatDB) DefaultPool {
+	return DefaultPool{
+		&db,
+		make(map[hash.Hash]bool),
+		NewPendingTxs(),
+		make(map[types.Address]arr_tx),
+		make(map[types.Address]QueenSortedTxs),
+	}
+}

56cc9ada79b350f9fd1189dc85d652699ad9b39f
cuit-lilang <lim92399@gmail.com>
2024-06-28 22:42:33 +0800

    Update address.go

--- types/address.go
+++ types/address.go
@@ -6,10 +6,21 @@ import (
 
 type Address [20]byte
 
+const AddressLength = 20
+const HashLen = 32
+
 func PubKeyToAddress(pub []byte) Address {
-	h := sha3.Keccak256(pub)
-	var addr Address
+	h := sha3.Keccak256(pub).Bytes()
+	addr := make([]byte, 20)
 	// TODO hash得到addr
-	copy(addr[:], h[len(h)-20:])
-	return addr
+	if len(h) > len(addr) {
+		h = h[len(h)-HashLen:]
+	} else {
+		copy(addr[HashLen-len(h):], h)
+	}
+	res := *new([20]byte)
+	for i, _ := range res {
+		res[i] = h[i]
+	}
+	return res
 }

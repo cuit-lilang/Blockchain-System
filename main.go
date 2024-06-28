213e9fc88c8ab840d36c8925c3e205d067b595e7
cuit-lilang <lim92399@gmail.com>
2024-06-28 22:43:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,72 @@
 package main
 
 import (
+	"cxchain223/blockchain"
 	"cxchain223/kvstore"
+	maker2 "cxchain223/maker"
+	"cxchain223/statdb"
+	"cxchain223/statemachine"
 	"cxchain223/trie"
-	"fmt"
+	"cxchain223/txpool"
+	"cxchain223/types"
 )
 
 func main() {
-	db := kvstore.NewLevelDB("./testdb")
-	state := trie.NewState(db, trie.EmptyHash)
-	state.Store([]byte("apple"), []byte("apple"))
-	state.Store([]byte("apply"), []byte("apply"))
-	state.Store([]byte("application"), []byte("application"))
-	state.Store([]byte("banana"), []byte("banana"))
-	state.Store([]byte("band"), []byte("band"))
-	value, err := state.Load([]byte("apple"))
-	fmt.Println(string(value), err)
+	//db := statdb.NewIStatDB(kvstore.NewLevelDB("./leveldb"), trie.EmptyHash)
+	Stadb := statdb.NewIStatDB("./Staleveldb")
+	datedb := trie.NewState(kvstore.NewLevelDB("./leveldb"), trie.EmptyHash)
+	txpol := txpool.NewDefaultPool(Stadb)
+	machine := statemachine.NewStateMachine()
+	rootHeader := blockchain.EmptyHeader
+	chain := blockchain.Blockchain{
+		&rootHeader,
+		//db.Trie,
+		datedb,
+		&txpol,
+	}
 
+	maker := maker2.NewBlockMaker(&txpol, &Stadb, &machine, &chain)
+
+	alice := types.NewAccount(nil, 10000000)
+	bob := types.NewAccount(nil, 10000000)
+	addr_alice := alice.GetAddress()
+	addr_bob := bob.GetAddress()
+	Stadb.Store(addr_alice, alice)
+	Stadb.Store(addr_bob, bob)
+	for i := 0; i < 10; i++ {
+		tx := types.NewTx(&alice, addr_bob, uint64(i+1), 1, 210000, 1, []byte("Send 1 eth."))
+		txpol.NewTx(tx)
+	}
+	for i := 0; i < 10; i++ {
+		tx := types.NewTx(&bob, addr_alice, uint64(i+1), 2, 200000, 1, []byte("Send 2 eth."))
+		txpol.NewTx(tx)
+	}
+	maker.Seal()
 }
+
+//	go GenerateTX(alice, addr_bob, txpol)
+//	maker.SealWithTime(3 * time.Minute)
+//	end := time.After(70 * time.Second)
+//	for {
+//
+//		select {
+//		case <-end:
+//			{
+//				return
+//			}
+//		}
+//	}
+//}
+//func GenerateTX(alice types.Account, addr_bob types.Address, txpol txpool.TxPool) {
+//	t := time.NewTicker(time.Second)
+//	end := time.After(time.Minute)
+//	for {
+//		select {
+//		case <-t.C:
+//			tx := types.NewTx(&alice, addr_bob, alice.Nonce+1, 1, 10, 1, []byte("Send 1 eth."))
+//			txpol.NewTx(tx)
+//		case <-end:
+//			return
+//		}
+//	}
+//}
